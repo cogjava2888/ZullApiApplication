a4900f51731fbb1995b371ed8a825cd3a3c3721c
cogjava2888 <kapoor.megha25@gmail.com>
2023-02-24 20:21:47 +0530

    Initial commit

---
+++ src/main/java/com/emarket/zuulapigateway/ZuulApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.emarket.zuulapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@EnableZuulProxy
+@SpringBootApplication
+//@CrossOrigin("*")
+public class ZuulApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApiGatewayApplication.class, args);
+	}
+
+}
